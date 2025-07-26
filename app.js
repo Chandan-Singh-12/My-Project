536753708b897d8db9db8ac48a048d7e1a559a58
Chandan-Singh-12 <chandansingh54648@gmail.com>
2025-07-26 19:41:06 +0530

    change to app.js

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-console.log("hello world")
+console.log("hello world");
+// This is a simple game application
+// Initialize the game
+function initGame() {
+    console.log("Game initialized");
+}
+// Start the game
+function startGame() {
+    console.log("Game started");
+}
+// End the game
+function endGame() {
+    console.log("Game ended");
+}       
+// Run the game
+initGame();
+startGame();
+// Simulate some game actions
+setTimeout(() => {
+    console.log("Game action performed");
+}, 2000);
+setTimeout(() => {  
+    endGame();
+}, 5000);
+// Export functions for potential use in other modules
+module.exports = {
+    initGame,
+    startGame,
+    endGame
+};      
